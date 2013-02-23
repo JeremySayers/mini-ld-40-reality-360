3ca28ce8f652426182acf616359c2164ce47a876
jeremy1080@gmail.com <jeremy1080@gmail.com@e4643346-6f54-7200-3a68-928734f37232>
2013-02-23 14:34:27 +0000

    Platform level object

---
+++ trunk/Reality360/src/com/reality360/Platform.java
@@ -0,0 +1,27 @@
+package com.reality360;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Platform extends Level {
+	@Override
+	public void paint(BufferedImage buffer, Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tick(BufferedImage buffer, Graphics g) {
+		paint(buffer, g);
+
+	}
+
+	@Override
+	public void init() {
+
+	}
+
+	public Platform() {
+
+	}
+}
