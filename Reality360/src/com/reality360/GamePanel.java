9a7430a6f248733a4603e66fa5db6bbd2bfdc5aa
bosrsf04 <bosrsf04@gmail.com>
2013-02-23 15:40:46 +0000

    Added: Key and Mouse Events Added: Sound Test

--- Reality360/src/com/reality360/GamePanel.java
+++ Reality360/src/com/reality360/GamePanel.java
@@ -2,11 +2,15 @@ package com.reality360;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
-import com.realith360.resource.Level;
+import com.reality360.resource.Level;
 
 public class GamePanel extends JPanel {
 	public static final BufferedImage BUFFER = new BufferedImage(Reality360.WIDTH, Reality360.HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -18,6 +22,30 @@ public class GamePanel extends JPanel {
             	tick();
             }
 		}, 1, 1000/60);
+		addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent arg0) {
+				if (level!=null) {
+					level.keyPressed(arg0);
+				}
+			}
+			public void keyReleased(KeyEvent arg0) {
+				if (level!=null) {
+					level.keyReleased(arg0);
+				}
+			}
+		});
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent arg0) {
+				if (level!=null) {
+					level.mousePressed(arg0);
+				}
+			}
+			public void mouseReleased(MouseEvent arg0) {
+				if (level!=null) {
+					level.mouseReleased(arg0);
+				}
+			}
+		});
 	}
 	public void paintComponent(Graphics g) {
 
---
+++ Reality360/src/com/reality360/resource/Entity.java
@@ -0,0 +1,16 @@
+package com.reality360.resource;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public abstract class Entity {
+	public abstract int getX();
+	public abstract int getY();
+	public abstract boolean isAlive();
+	public abstract void keyPressed(KeyEvent e);
+	public abstract void keyReleased(KeyEvent e);
+	public abstract void mousePressed(MouseEvent e);
+	public abstract void mouseReleased(MouseEvent e);
+	public abstract void paint(Graphics g);
+	public abstract void tick();
+}
