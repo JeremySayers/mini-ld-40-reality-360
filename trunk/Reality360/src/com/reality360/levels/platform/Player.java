fe13b6c7234b0b73b848f176f302c4cacfa5527f
jeremy1080@gmail.com <jeremy1080@gmail.com@e4643346-6f54-7200-3a68-928734f37232>
2013-02-23 15:46:30 +0000

    Fixed Player object

---
+++ trunk/Reality360/src/com/reality360/levels/platform/Player.java
@@ -0,0 +1,62 @@
+package com.reality360.levels.platform;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+
+public class Player extends com.reality360.resource.Entity{
+	int x, y, height, width, xspeed, yspeed, jumprate;
+	boolean isAlive, isJumping;
+	final int X_START = 50;
+	final int Y_START = 550;
+	public Player(){
+		x = X_START;
+		y = Y_START;
+	}
+	public void paint(Graphics g) {
+		
+	}
+
+	public void tick(Graphics g) {
+		
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+	public int getY() {
+		return this.y;
+	}
+
+	public boolean isAlive() {
+		return false;
+	}
+
+	public void tick() {
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
