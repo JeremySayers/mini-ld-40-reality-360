8232f0968757fb45286724eae4fa1d01962aed2c
spencer.rak <spencer.rak@gmail.com>
2013-02-23 15:01:55 +0000

    yolo

--- Reality360/src/com/realith360/resource/Entity.java
+++ Reality360/src/com/realith360/resource/Entity.java
@@ -2,6 +2,7 @@ package com.realith360.resource;
 import java.awt.Graphics;
 
 public abstract class Entity {
+
 	public abstract int getX();
 	public abstract int getY();
 	public abstract boolean isAlive();
--- Reality360/src/com/reality360/levels/platform/Platform.java
+++ Reality360/src/com/reality360/levels/platform/Platform.java
@@ -5,14 +5,30 @@ import java.awt.image.BufferedImage;
 
 import com.realith360.resource.Level;
 
+
 public class Platform extends Level {
+	public void paint(BufferedImage buffer, Graphics g) {
+		
+
+	}
+	public void tick(BufferedImage buffer, Graphics g) {
+		paint(buffer, g);
+
+	}
+	public void init() {
+
+	}
 	public Platform() {
 		
 	}
+	@Override
 	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
 		
 	}
+	@Override
 	public void tick() {
+		// TODO Auto-generated method stub
 		
 	}
 }
