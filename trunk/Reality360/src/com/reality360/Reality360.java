5e09a19265d92de718e9ab45dcd8f9d82d80c9e0
bosrsf04@gmail.com <bosrsf04@gmail.com@e4643346-6f54-7200-3a68-928734f37232>
2013-02-23 14:04:21 +0000

    Project Creation

---
+++ trunk/Reality360/src/com/reality360/Reality360.java
@@ -0,0 +1,12 @@
+package com.reality360;
+
+import java.applet.Applet;
+
+public class Reality360 extends Applet {
+	public static void main(String[] args) {
+		new Reality360();
+	}
+	public Reality360() {
+		
+	}
+}
