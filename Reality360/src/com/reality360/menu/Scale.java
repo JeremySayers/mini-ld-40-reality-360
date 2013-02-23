be8f44cc4042a119df9ebef5077b8cb213c4b6d5
bosrsf04 <bosrsf04@gmail.com>
2013-02-23 22:09:15 +0000

    Added: Credits

--- Reality360/src/com/reality360/menu/Scale.java
+++ Reality360/src/com/reality360/menu/Scale.java
@@ -3,9 +3,9 @@ package com.reality360.menu;
 public class Scale {
 	
 	public static final int SCREEN_WIDTH = 800;
-	public static final int SCREEN_HEIGHT = 600;
+	public static final int SCREEN_HEIGHT = 800;
 	
 	public static final int LETTER_IMG_WIDTH=SCREEN_WIDTH/20;
 	public static final int LETTER_IMG_HEIGHT=SCREEN_WIDTH/20;
-	public static final int MENU_SPACE=20;
+	public static final int MENU_SPACE=30;
 }
