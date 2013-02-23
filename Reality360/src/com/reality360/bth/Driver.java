448d8b839abd1e2b98352c6d06bcb0255922ac82
spencer.rak <spencer.rak@gmail.com>
2013-02-23 16:09:11 +0000

    BulletHell package created

---
+++ Reality360/src/com/reality360/bth/Driver.java
@@ -0,0 +1,30 @@
+package com.reality360.bth;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import com.reality360.resource.Level;
+
+public class Driver extends Level{
+
+	
+	public void paint(Graphics g) {
+		
+	}
+	public void tick() {
+		
+	}
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	public void keyReleased(KeyEvent e) {
+		
+	}
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+}
---
+++ Reality360/src/com/reality360/bth/Enemy.java
@@ -0,0 +1,24 @@
+package com.reality360.bth;
+
+import java.awt.Graphics;
+
+import com.reality360.resource.AI;
+
+public class Enemy extends AI{
+
+	public int getX() {
+		return 0;
+	}
+	public int getY() {
+		return 0;
+	}
+	public boolean isAlive() {
+		return false;
+	}
+	public void paint(Graphics g) {
+		
+	}
+	public void tick() {
+		
+	}
+}
