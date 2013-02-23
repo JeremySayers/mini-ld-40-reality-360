6abdf6c3a7b7b01abd89adbc74e1ba135bd86903
bosrsf04@gmail.com <bosrsf04@gmail.com@e4643346-6f54-7200-3a68-928734f37232>
2013-02-23 20:33:43 +0000

    Added: Intro Now Opens To Menu To Do: Make Menu

--- trunk/Reality360/src/com/reality360/intro/Intro.java
+++ trunk/Reality360/src/com/reality360/intro/Intro.java
@@ -4,14 +4,18 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 import com.reality360.Reality360;
+import com.reality360.menu.Words;
 import com.reality360.resource.Level;
 
 public class Intro extends Level {
-	private int tick = 0;
+	private int tick;
+	private static final Image menu = Reality360.loadImage("/title.jpg");
 	public Intro() {
 		tick = 0;
 	}
@@ -93,6 +97,34 @@ public class Intro extends Level {
 			g.drawString("[", x, y);
 			x += fm.stringWidth("[")+fm.stringWidth("M")/2-fm.stringWidth(msg)/2;
 			g.drawString(msg, x, y);
+			
+			if (t>0) {
+				g.setColor(Color.BLACK);
+				int h = (int)(Reality360.HEIGHT/2*(t/100.0));
+				g.fillRect(0, 0, Reality360.WIDTH, h);
+				g.fillRect(0, Reality360.HEIGHT-h, Reality360.WIDTH, h);
+				t -= 100;
+				if (t>0) {
+					g.setColor(Color.GREEN);
+					int w = (int)(Reality360.WIDTH*(t/200.0));
+					g.fillRect(Reality360.WIDTH/2-w/2, Reality360.HEIGHT/2-5, w, 10);
+					if (w>=Reality360.WIDTH) {
+						t -= 200;
+						g.drawImage(menu, 0, 0, null);
+						h = (int)(Reality360.HEIGHT/2 - Reality360.HEIGHT/2*(t/100.0));
+						g.setColor(Color.BLACK);
+						g.fillRect(0, 0, Reality360.WIDTH, h-5);
+						g.fillRect(0, Reality360.HEIGHT-h+5, Reality360.WIDTH, h);
+						g.setColor(Color.GREEN);
+						g.fillRect(0, h-5, Reality360.WIDTH, 5);
+						g.fillRect(0, Reality360.HEIGHT-h, Reality360.WIDTH, 5);
+					} else if (t%30>10) {
+						g.setColor(Color.WHITE);
+						BufferedImage word = Words.menuWord("Initializing...", 35, 35);
+						g.drawImage(word, Reality360.WIDTH/2-word.getWidth()/2, Reality360.HEIGHT/2-100, null);
+					}
+				}
+			}
 		} else if (tick%d>d/2) {
 			g.drawString("\u258D", x, y);
 		}
