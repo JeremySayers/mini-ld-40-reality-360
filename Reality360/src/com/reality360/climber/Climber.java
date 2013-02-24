a6086d43c57436520be4998d6bb30229fcc80c2f
bosrsf04 <bosrsf04@gmail.com>
2013-02-24 07:47:24 +0000

    Added: Basic Climbing Level - No Actual Game Play Yet

--- Reality360/src/com/reality360/climber/Climber.java
+++ Reality360/src/com/reality360/climber/Climber.java
@@ -11,21 +11,54 @@ public class Climber extends Level {
 	private int tick = 0;
 	public static Tile[][] tiles = new Tile[16][20];
 	public static int offset = 0;
+	public static int speed = 20;
 	public Climber() {
 		for (int i=0; i<16; i++) {
 			tiles[i] = new Tile[20];
 		}
-		tiles[14][11] = new Tile();
-		tiles[14][10] = new Tile();
-		tiles[14][9] = new Tile();
-		player.move(400, 490);
-		for (int r=12; r>0; r--) {
-			for (int c=1; c<19; c++) {
-				if (Math.random()>0.9) {
-					tiles[r][c-1] = new Tile();
-					tiles[r][c] = new Tile();
-					tiles[r][c+1] = new Tile();
-					c+=2;
+		tiles[14][11] = createTile();
+		tiles[14][10] = createTile();
+		tiles[14][9] = createTile();
+		for (int r=12; r>=0; r-=2) {
+			generate(r);
+		}
+	}
+	private Tile createTile() {
+		return new Tile(Math.random()>0.75?3*speed/2:0);
+	}
+	public void generate(int row) {
+		int li = 0;
+		for (int c=0; c<20; c++) {
+			if (tiles[row+2][c]!=null) {
+				li = c;
+				break;
+			}
+		}
+		int ri = li;
+		while (ri+1!=20 && tiles[row+2][ri+1]!=null) {
+			ri++;
+		}
+		for (int c=0; c<20; c++) {
+			tiles[row][c] = null;
+		}
+		if (ri!=li) {
+			int len = (int)(Math.random()*2)+2;
+			int sep = (int)(Math.random()*3);
+			double ty = Math.random();
+			if (ty<0.3) {
+				int start = Math.min(19-len, Math.max(0, li-sep-len));
+				for (int c=0;c<len;c++) {
+					tiles[row][start+c] = createTile();
+				}
+			} else if (ty<0.6) {
+				int start = Math.min(19, Math.max(len, Math.min(li-len-sep, li+sep+len)));
+				for (int c=0;c<len;c++) {
+					tiles[row][start-c] = createTile();
+				}
+			} else {
+				int start = Math.max(len, Math.min(19, ri+sep+len));
+				for (int c=0;c<len;c++) {
+					tiles[row][start-c] = createTile();
 				}
 			}
 		}
@@ -46,7 +79,7 @@ public class Climber extends Level {
 		for (int r=0; r<16; r++) {
 			for (int c=0; c<20; c++) {
 				if (tiles[r][c]!=null) {
-					tiles[r][c].move(40*c, 40*(r-1)+tick%40);
+					tiles[r][c].move(40*c, 40*(r-1)+tick*40/speed);
 					tiles[r][c].paint(g);
 				}
 			}
@@ -54,25 +87,17 @@ public class Climber extends Level {
 		player.paint(g);
 	}
 	public void tick() {
-		player.move(player.getX(), player.getY()+tick%40);
-		player.tick();
-		tick++;
-		tick%=40;
-		if (tick%40==0) {
-			for (int r=15; r>0; r--) {
-				for (int c=0; c<20; c++) {
-					tiles[r][c] = tiles[r-1][c];
+		if (player.isAlive()) {
+			player.tick();
+			tick++;
+			tick%=speed;
+			if (tick==0) {
+				for (int r=15; r>0; r--) {
+					for (int c=0; c<20; c++) {
+						tiles[r][c] = tiles[r-1][c];
+					}
 				}
-			}
-			for (int c=1; c<19; c++) {
-				if (Math.random()>0.95) {
-					tiles[0][c-1] = new Tile();
-					tiles[0][c] = new Tile();
-					tiles[0][c+1] = new Tile();
-					c+=2;
-				} else {
-					tiles[0][c] = null;
-				}
+				generate(0);
 			}
 		}
 	}
